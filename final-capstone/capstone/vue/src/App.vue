145479d8bad91083a53b4c95b472256fdc2e71cb
Lauren McCuen <laurenemccuen@gmail.com>
2023-06-01 15:06:44 -0400

    Updated applications form data

--- final-capstone/capstone/vue/src/App.vue
+++ final-capstone/capstone/vue/src/App.vue
@@ -7,30 +7,46 @@
         class="pr-logo"
       />
       <nav>
-        <router-link class="button-link" v-bind:to="{ name: 'home' }">Home</router-link
+        <router-link class="button-link" v-bind:to="{ name: 'home' }"
+          >Home</router-link
         >&nbsp;
-        <router-link class="button-link" v-bind:to="{ name: 'volunteer' }">Volunteer</router-link
+        <router-link class="button-link" v-bind:to="{ name: 'volunteer' }"
+          >Volunteer</router-link
         >&nbsp;
-        <router-link class="button-link" v-bind:to="{ name: 'login' }">Login </router-link>
+        <!-- <router-link class="button-link" v-bind:to="{ name: 'login' }"
+          >Login
+        </router-link>
+        &nbsp; -->
+        <router-link class="button-link" v-bind:to="{ name: 'register' }"
+          >Register
+        </router-link>
         &nbsp;
-        <router-link class="button-link" v-bind:to="{ name: 'register' }">Register </router-link>
-        &nbsp;
-        <router-link class="button-link"
-          v-bind:to="{ name: 'logout' }"
-          v-if="$store.state.token != ''"
-          >Logout</router-link
-        >
+        <router-link class="button-link" v-bind:to="{ name: 'login' }">
+          {{ loginButtonText }}
+        </router-link>
       </nav>
     </div>
- 
+
     <router-view />
- </div>
- 
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    isLoggedIn() {
+      return this.$store.state.token !== "";
+    },
+    loginButtonText() {
+      return this.isLoggedIn ? "Login" : "Logout";
+    },
+  },
+};
+</script>
 
 <style>
 #app {
-  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
   background-color: #b2d8f5;
   min-height: 100vh;
 }
@@ -58,18 +74,20 @@ header {
 .button-link {
   font-weight: bold;
   text-decoration: none;
-  background-color: #ED815A;
+  background-color: #ed815a;
   padding: 4px 8px;
   border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.8); 
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.8);
   color: #055983;
   cursor: pointer;
 }
 
-.button-link:hover{
-  background-color: #ED815A;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.8);
+.button-link:last-child {
+  margin-right: 10px;
 }
 
-
+.button-link:hover {
+  background-color: #ed815a;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.8);
+}
 </style>
